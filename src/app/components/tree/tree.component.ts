095bb0381dba66a1d814500f3f4149f11aa5a85d
Charles Kornoelje <charleskornoelje@gmail.com>
2020-12-04 20:55:27 -0500

    create tree service

--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// TODO: is this where I import it?
+import { TreeService } from 'src/app/services/tree.service';
+
 @Component({
   selector: 'app-tree',
   templateUrl: './tree.component.html',
@@ -7,38 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeComponent implements OnInit {
   // TODO: will need to add polyfill for mobile to work: https://angular2-tree.readme.io/v8.2.0/docs/drag-drop#mobile
-
-  // TODO: make this dynamic from db.
-  nodes = [
-    {
-      id: 1,
-      name: 'Tag: Fruit',
-      children: [
-        { id: 2, name: 'Strawberry' },
-        { id: 3, name: 'Water Melon' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'Tag: Meat',
-      children: [
-        { id: 5, name: 'Cooked Chicken' },
-        {
-          id: 6,
-          name: 'Tag: Cow Related',
-          children: [
-            { id: 7, name: 'Hamburgers' }
-          ]
-        }
-      ]
-    }
-  ];
-  options = {
+  nodes = [];
+  treeOptions = {
     allowDrag: true,
     allowDrop: true,
   };
 
-  constructor() { }
+  constructor(treeSvc: TreeService) {
+    this.nodes = treeSvc.getNodes()
+  }
 
   ngOnInit(): void {
   }
