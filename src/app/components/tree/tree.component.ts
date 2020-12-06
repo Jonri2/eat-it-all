1fcadf9a3b7ffe9c13b6ee8a17f4a6cab0ea1546
Charles Kornoelje <charleskornoelje@gmail.com>
2020-12-06 15:16:16 -0500

    add fuzzy search from docs

--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TreeModel, TreeNode } from '@circlon/angular-tree-component';
 
 // TODO: is this where I import it?
 import { TreeService } from 'src/app/services/tree.service';
@@ -18,6 +19,10 @@ export class TreeComponent implements OnInit {
     animateAcceleration: 1.3,
   };
 
+  filterTree = (value: string, treeModel: TreeModel) => {
+    treeModel.filterNodes((node: TreeNode) => fuzzySearch(value, node.data.name));
+  }
+
   constructor(treeSvc: TreeService) {
     this.nodes = treeSvc.getNodes()
   }
@@ -25,4 +30,31 @@ export class TreeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}
+
+/* ref: https://github.com/CirclonGroup/angular-tree-component/blob/master/projects/example-app/src/app/filter/filter.component.ts */
+const fuzzySearch = (needle: string, haystack: string) => {
+  const haystackLC = haystack.toLowerCase();
+  const needleLC = needle.toLowerCase();
+
+  const hLen = haystack.length;
+  const nLen = needleLC.length;
+
+  if (nLen > hLen) {
+    return false;
+  }
+  if (nLen === hLen) {
+    return needleLC === haystackLC;
+  }
+  outer: for (let i = 0, j = 0; i < nLen; i++) {
+    const nch = needleLC.charCodeAt(i);
+
+    while (j < hLen) {
+      if (haystackLC.charCodeAt(j++) === nch) {
+        continue outer;
+      }
+    }
+    return false;
+  }
+  return true;
 }
