db8b388d3f90055fdac53dc4a60f657fb11d504b
Jonathan Ellis <jde27@students.calvin.edu>
2020-12-15 12:34:24 -0500

    comment out test code

--- src/app/services/tree.service.ts
+++ src/app/services/tree.service.ts
@@ -77,36 +77,36 @@ export class TreeService {
       this._nodes = res.nodes;
     });
     // Run this to reset the db
-    this.getUserDoc().set({
-      nodes: [
-        {
-          id: 1,
-          name: 'Fruit',
-          children: [
-            { id: 2, name: '🍎 Apple' },
-            { id: 8, name: '🍋 Lemon' },
-            { id: 9, name: '🍋🟩 Lime' },
-            { id: 10, name: '🍊 Orange' },
-            { id: 11, name: '🍓 Strawberry' },
-          ],
-          isTag: true,
-        },
-        {
-          id: 4,
-          name: 'Meat',
-          children: [
-            { id: 5, name: '🐔 Cooked Chicken' },
-            {
-              id: 6,
-              name: '🐄 Cow Related',
-              children: [{ id: 7, name: '🍔 Hamburger' }],
-              isTag: true,
-            },
-          ],
-          isTag: true,
-        },
-      ],
-    });
+    // this.getUserDoc().set({
+    //   nodes: [
+    //     {
+    //       id: 1,
+    //       name: 'Fruit',
+    //       children: [
+    //         { id: 2, name: '🍎 Apple' },
+    //         { id: 8, name: '🍋 Lemon' },
+    //         { id: 9, name: '🍋🟩 Lime' },
+    //         { id: 10, name: '🍊 Orange' },
+    //         { id: 11, name: '🍓 Strawberry' },
+    //       ],
+    //       isTag: true,
+    //     },
+    //     {
+    //       id: 4,
+    //       name: 'Meat',
+    //       children: [
+    //         { id: 5, name: '🐔 Cooked Chicken' },
+    //         {
+    //           id: 6,
+    //           name: '🐄 Cow Related',
+    //           children: [{ id: 7, name: '🍔 Hamburger' }],
+    //           isTag: true,
+    //         },
+    //       ],
+    //       isTag: true,
+    //     },
+    //   ],
+    // });
   };
 
   hasTag = (tag: string, node?: Node): boolean => {
